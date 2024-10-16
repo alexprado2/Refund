75060b3c88dfa633d98ccff5d82615e7142f931a
DEVAlexHenrique <alex.henrique1212@gmail.com>
2024-10-16 16:53:28 -0300

    submit event expenses

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 //select form elements
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 //capture input event to format value
 amount.oninput = () => {
@@ -20,3 +23,17 @@ function formatCurrencyBRL(value) {
     return value
 }
 
+//capture the submit event to obtain the values
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    //object with new expenses
+    const newExpense = {
+        id:new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+}
